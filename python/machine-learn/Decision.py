c4c0009118a0fa7d394413a34d2b0a1680662b72
IllalRajinCoding <roxyuciha@gmail.com>
2025-08-28 03:59:32 +0700

    push

--- python/machine-learn/Decision.py
+++ python/machine-learn/Decision.py
@@ -2,4 +2,4 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score,confusion_matrix
 
-  
+print("Hello, World!")
